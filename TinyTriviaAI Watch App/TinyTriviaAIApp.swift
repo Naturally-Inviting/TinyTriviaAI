a738c5115161d2f68ff73b2cf6071f6818e5d28d
Will Brandin <will.brandin@humminghomes.com>
2023-03-15 14:17:08 -0600

    Initial Commit

---
+++ TinyTriviaAI Watch App/TinyTriviaAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTriviaAIApp.swift
+//  TinyTriviaAI Watch App
+//
+//  Created by Will Brandin on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TinyTriviaAI_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
